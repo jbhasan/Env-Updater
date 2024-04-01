69f718f2cb0ea1c3e044fca4d19216e47bec52e0
Hasan <jbhasan@gmail.com>
2024-04-01 11:13:36 +0600

    Update text

--- src/resources/views/update-env.blade.php
+++ src/resources/views/update-env.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>ENV UPDATER :: UPDATE</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link href='https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.42.2/codemirror.min.css' rel='stylesheet'>
 	<link href='https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.42.2/theme/duotone-light.min.css' rel='stylesheet'>
